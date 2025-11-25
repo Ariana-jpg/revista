7ec3bb52c7c7e67d926eabf862584124a20637e1
Ariana <tu-correo-de-github>
2025-11-25 16:33:36 -0500

    holis

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,88 @@
 // npm install react-router-dom
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
-// Firebase (ya inicializado en ./firebase)
-import "./App.css";
+import './App.css'
 
 // Layout
-import Header from "./components/header.jsx";
-import Hero from "./components/hero.jsx";
-import Footer from "./components/footer.jsx";
+import Header from './components/header.jsx'
+import Hero from './components/hero.jsx'
+import Footer from './components/footer.jsx'
 
 // Componentes principales
-import Contenido from "./components/contenido.jsx";
-import Contacto from "./pages/contacto.jsx";
-import Comunidad from "./components/comunidad.jsx";
+import Contenido from './components/contenido.jsx'
+import Contacto from './pages/contacto.jsx'
+import Comunidad from './components/comunidad.jsx'
 
 // Páginas protegidas
-import Ropa from "./components/ropa.jsx";
-import Maquillaje from "./components/maquillaje.jsx";
-import Skincare from "./components/skincare.jsx";
+import Ropa from './components/ropa.jsx'
+import Maquillaje from './components/maquillaje.jsx'
+import Skincare from './components/skincare.jsx'
 
-// Sistema de login
-import Login from "./components/Login.jsx";
-import RegistrarCuenta from "./components/RegistrarCuenta.jsx";
+// Login y registro
+import Login from './components/Login.jsx'
+import RegistrarCuenta from './components/RegistrarCuenta.jsx'
 
-// Ruta privada
-import RutaPrivada from "./components/RutaPrivada.jsx";
+// Rutas privadas
+import RutaPrivada from './components/RutaPrivada.jsx'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Hero />
+    return (
+        <HashRouter>
+            <Header />
+            <Hero />
 
-      <Routes>
+            <Routes>
+                {/* Página principal */}
+                <Route path="/" element={<Contenido />} />
 
-        {/* Página principal */}
-        <Route path="/" element={<Contenido />} />
+                {/* Contacto (pública) */}
+                <Route path="/contacto" element={<Contacto />} />
 
-        {/* Contacto (pública) */}
-        <Route path="/contacto" element={<Contacto />} />
+                {/* 🔒 RUTAS PRIVADAS */}
+                <Route
+                    path="/maquillaje"
+                    element={
+                        <RutaPrivada>
+                            <Maquillaje />
+                        </RutaPrivada>
+                    }
+                />
 
-        {/* 🔒 RUTAS PRIVADAS */}
-        <Route
-          path="/maquillaje"
-          element={
-            <RutaPrivada>
-              <Maquillaje />
-            </RutaPrivada>
-          }
-        />
-        <Route
-          path="/skincare"
-          element={
-            <RutaPrivada>
-              <Skincare />
-            </RutaPrivada>
-          }
-        />
-        <Route
-          path="/ropa"
-          element={
-            <RutaPrivada>
-              <Ropa />
-            </RutaPrivada>
-          }
-        />
-        <Route
-          path="/comunidad"
-          element={
-            <RutaPrivada>
-              <Comunidad />
-            </RutaPrivada>
-          }
-        />
+                <Route
+                    path="/skincare"
+                    element={
+                        <RutaPrivada>
+                            <Skincare />
+                        </RutaPrivada>
+                    }
+                />
 
-        {/* Login y registro */}
-        <Route path="/iniciar-sesion" element={<Login />} />
-        <Route path="/registrarse" element={<RegistrarCuenta />} />
+                <Route
+                    path="/ropa"
+                    element={
+                        <RutaPrivada>
+                            <Ropa />
+                        </RutaPrivada>
+                    }
+                />
 
-      </Routes>
+                <Route
+                    path="/comunidad"
+                    element={
+                        <RutaPrivada>
+                            <Comunidad />
+                        </RutaPrivada>
+                    }
+                />
 
-      <Footer />
-    </BrowserRouter>
-  );
+                {/* Login y registro */}
+                <Route path="/iniciar-sesion" element={<Login />} />
+                <Route path="/registrarse" element={<RegistrarCuenta />} />
+            </Routes>
+
+            <Footer />
+        </HashRouter>
+    )
 }
 
-export default App;
+export default App
