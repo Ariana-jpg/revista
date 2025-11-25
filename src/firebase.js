e83544f111aabb356d0bbfd06a0eac29f1fd847e
Ariana <tu-correo-de-github>
2025-11-25 16:28:13 -0500

    revista

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,26 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAc_b73lE-_XBjw4CnHzKvopeKCid0AzJc",
-  authDomain: "revista-c59b8.firebaseapp.com",
-  projectId: "revista-c59b8",
-  storageBucket: "revista-c59b8.firebasestorage.app",
-  messagingSenderId: "275495484100",
-  appId: "1:275495484100:web:efe13b0153d361728b2791"
-};
+    apiKey: 'AIzaSyAc_b73lE-_XBjw4CnHzKvopeKCid0AzJc',
+    authDomain: 'revista-c59b8.firebaseapp.com',
+    projectId: 'revista-c59b8',
+    storageBucket: 'revista-c59b8.firebasestorage.app',
+    messagingSenderId: '275495484100',
+    appId: '1:275495484100:web:efe13b0153d361728b2791',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
 
-//exportaciones
-export default app;
-export { db, getAuth };
+// Initialize Firestore
+const db = getFirestore(app)
+
+// Initialize Auth
+const auth = getAuth(app)
+
+// exportaciones
+export { app, db, auth }
